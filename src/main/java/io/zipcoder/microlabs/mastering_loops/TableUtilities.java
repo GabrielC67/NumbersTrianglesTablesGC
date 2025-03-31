414585b494dc67f5a1d9fc8f5df1e524dd65ca19
GabrielC67 <gabecruzpro@gmail.com>
2025-03-31 15:31:04 -0400

    finished

--- src/main/java/io/zipcoder/microlabs/mastering_loops/TableUtilities.java
+++ src/main/java/io/zipcoder/microlabs/mastering_loops/TableUtilities.java
@@ -2,8 +2,20 @@ package io.zipcoder.microlabs.mastering_loops;
 
 public class TableUtilities {
     public static String getMultiplicationTable(int tableSize) {
+        StringBuilder stringBuilder = new StringBuilder();
+        String separator = " |";
 
-        return null;
+        for (int i = 1; i <= tableSize; i++) {
+            for (int j = 1; j <= tableSize; j++) {
+                stringBuilder.append(String.format("%3d", (i * j)));
+                if (j <= tableSize) {
+                    stringBuilder.append(separator);
+                }
+            }
+            stringBuilder.append("\n");
+        }
+        String table = stringBuilder.toString();
+        return table;
     }
 
     public static String getSmallMultiplicationTable() {
